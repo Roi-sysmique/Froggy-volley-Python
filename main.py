4e0b38a6d8bf97549e2b36274df8e7729ed88511
Roi-sysmique <elhechmihamza@gmail.com>
2024-04-14 11:54:15 +0100

    'Ball-Physique'

--- main.py
+++ main.py
@@ -14,6 +14,28 @@ net_rect = net.get_rect(midbottom=(SCREEN_WIDTH/2, SCREEN_HEIGHT))
 net.fill('green')
 
 
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.gravity = 0
+        height, width = 30, 30
+        self.x_velocity = 5
+        self.image = pygame.surface.Surface((width, height))
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        self.image.fill('yellow')
+    def update(self):
+        self.gravity += 0.7
+        self.rect.bottom += self.gravity
+        self.rect.centerx += self.x_velocity
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+            self.gravity = 0
+        if self.rect.clipline((net_rect.x, net_rect.y), net_rect.topright) and self.gravity == abs(self.gravity):
+            self.gravity = -15
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -39,9 +61,9 @@ class Player(pygame.sprite.Sprite):
 
     def movement(self, keys):
         if keys[pygame.K_LEFT]:
-            self.rect.left -= 5
+            self.rect.left -= 7
         if keys[pygame.K_RIGHT]:
-            self.rect.right += 5
+            self.rect.right += 7
         if keys[pygame.K_UP] and self.rect.bottom >= SCREEN_HEIGHT:
             self.gravity = -10
 
@@ -71,15 +93,33 @@ class Player2(pygame.sprite.Sprite):
 
     def movement(self, keys):
         if keys[pygame.K_q]:
-            self.rect.left -= 5
+            self.rect.left -= 7
         if keys[pygame.K_d]:
-            self.rect.right += 5
+            self.rect.right += 7
         if keys[pygame.K_z] and self.rect.bottom >= SCREEN_HEIGHT:
             self.gravity = -10
 
 
 player = Player()
 player2 = Player2()
+ball = Ball()
+
+
+def player_collision(keys):
+    if ball.rect.colliderect(player2):
+        ball.gravity = -20
+        if keys[pygame.K_q]:
+            ball.x_velocity = -ball.x_velocity - 2
+        elif not keys[pygame.K_q]:
+            ball.x_velocity = -ball.x_velocity + 0.5
+    if ball.rect.colliderect(player):
+        ball.gravity = -20
+        if keys[pygame.K_RIGHT]:
+            ball.x_velocity = abs(ball.x_velocity) + 2
+        elif not keys[pygame.K_RIGHT]:
+            ball.x_velocity = abs(ball.x_velocity) - 0.5
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -87,6 +127,7 @@ while True:
             quit()
 
     keys_input = pygame.key.get_pressed()
+    player_collision(keys_input)
     screen.blit(background, (0, 0))
     screen.blit(player.image, player.rect)
     screen.blit(net, net_rect)
@@ -95,5 +136,7 @@ while True:
     screen.blit(player2.image, player2.rect)
     player2.movement(keys_input)
     player2.update()
+    screen.blit(ball.image, ball.rect)
+    ball.update()
     pygame.display.update()
     clock.tick(65)
